893e6552d8f51fb5328bfbac082d2e65dbead830
nguynkhn <nguynkhan479@gmail.com>
2026-09-25 08:28:53 +0700

    multiple submissions

--- submit.go
+++ submit.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"slices"
 	"strings"
 	"time"
 )
@@ -36,9 +35,9 @@ type SubmissionResponse struct {
 	Submission Submission `json:"submission"`
 }
 
-func (c *Client) Submit(courseId, questionId, compilerId int, sourceCode string) (int, error) {
+func (c *Client) Submit(courseID, questionID, compilerID int, sourceCode string) (int, error) {
 	submitReq, err := c.NewRequest("POST", "/submissions", &SubmitRequest{
-		CourseID: courseId, QuestionID: questionId, CompilerID: compilerId,
+		CourseID: courseID, QuestionID: questionID, CompilerID: compilerID,
 		SourceCode: sourceCode,
 	})
 	if err != nil {
@@ -53,10 +52,10 @@ func (c *Client) Submit(courseId, questionId, compilerId int, sourceCode string)
 	return submitResp.ID, err
 }
 
-func (c *Client) Submission(submissionId int) (Submission, error) {
+func (c *Client) Submission(submissionID int) (Submission, error) {
 	var submission Submission
 
-	endpoint := fmt.Sprintf("/submissions/%d", submissionId)
+	endpoint := fmt.Sprintf("/submissions/%d", submissionID)
 	submissionReq, err := c.NewRequest[any]("GET", endpoint, nil)
 	if err != nil {
 		return submission, err
@@ -106,6 +105,14 @@ func pollSubmission(c *Client, submissionId int) {
 	}
 }
 
+func indexProblemByCode(problems []Problem) map[string]int {
+	m := make(map[string]int, len(problems))
+	for _, problem := range problems {
+		m[strings.ToLower(problem.Code)] = problem.ID
+	}
+	return m
+}
+
 func SubmitCommand(c *Client) {
 	courseID, ok := c.GetCourse()
 	if !ok {
@@ -118,61 +125,47 @@ func SubmitCommand(c *Client) {
 		return
 	}
 
-	path := os.Args[2]
-	lang, ok := DetectLanguage(path)
-	if !ok {
-		fmt.Println("No available language detected")
-		return
-	}
-
-	compilers, err := c.Compilers(courseID)
-	if err != nil {
-		fmt.Println("Failed to retrieve compilers:", err)
-		return
-	}
-
-	compilerIdx := slices.IndexFunc(compilers, func(compiler Compiler) bool {
-		return compiler.ID == int(lang)
-	})
-	if compilerIdx == -1 {
-		fmt.Println("This course does not include this language")
-		return
-	}
-	compilerID := compilers[compilerIdx].ID
-
-	srcBytes, err := os.ReadFile(path)
-	if err != nil {
-		fmt.Println("Failed to read file:", err)
-		return
-	}
-	sourceCode := string(srcBytes)
-
-	problemCode := strings.TrimSuffix(path, filepath.Ext(path))
-	if comment, ok := ExtractFirstComment(sourceCode, lang); ok {
-		problemCode = comment
-	}
-
 	problems, err := c.Problems(courseID)
 	if err != nil {
 		fmt.Println("Failed to retrieve problems:", err)
 		return
 	}
+	problemMap := indexProblemByCode(problems)
 
-	problemIdx := slices.IndexFunc(problems, func(problem Problem) bool {
-		return problem.Code == problemCode
-	})
-	if problemIdx == -1 {
-		fmt.Println("No problem with this code found")
-		return
+	for _, path := range os.Args[2:] {
+		srcBytes, err := os.ReadFile(path)
+		if err != nil {
+			fmt.Println("Failed to read file:", err)
+			continue
+		}
+		src := string(srcBytes)
+
+		lang, ok := DetectLanguage(path)
+		if !ok {
+			fmt.Println("No available language detected")
+			continue
+		}
+
+		fileName := filepath.Base(path)
+		problemCode, ok := ExtractFirstComment(src, lang)
+		if !ok {
+			problemCode = strings.TrimSuffix(fileName, filepath.Ext(fileName))
+		}
+
+		problemID, ok := problemMap[strings.ToLower(problemCode)]
+		if !ok {
+			fmt.Println("No problem with this code found")
+			continue
+		}
+
+		compilerID := int(lang)
+		submissionID, err := c.Submit(courseID, problemID, compilerID, src)
+		if err != nil {
+			fmt.Println("Failed to submit code:", err)
+			continue
+		}
+
+		fmt.Println(fileName, "submitted! Retrieving status...")
+		pollSubmission(c, submissionID)
 	}
-	problemID := problems[problemIdx].ID
-
-	submissionId, err := c.Submit(courseID, problemID, compilerID, sourceCode)
-	if err != nil {
-		fmt.Println("Failed to submit code:", err)
-		return
-	}
-
-	fmt.Println("Code submitted! Retrieving status...")
-	pollSubmission(c, submissionId)
 }
